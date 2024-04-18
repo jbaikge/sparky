89fe49afae7ab8a5977ef9ba13317b31766fea43
jake <bigjake2s@gmail.com>
2024-04-18 13:09:15 -0400

    feat: testing out pulling user from context

--- handlers/dashboard.go
+++ handlers/dashboard.go
@@ -4,10 +4,14 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+
+	"github.com/jbaikge/sparky/models/user"
+	"github.com/jbaikge/sparky/modules/middleware"
 )
 
 func dashboard(w http.ResponseWriter, r *http.Request) {
+	user := r.Context().Value(middleware.ContextAdminUser).(*user.User)
 	var buf bytes.Buffer
-	fmt.Fprintf(&buf, "Welcome to the dashboard")
+	fmt.Fprintf(&buf, "Welcome to the dashboard, %s", user.Name())
 	w.Write(buf.Bytes())
 }
