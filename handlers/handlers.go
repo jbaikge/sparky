4289b42d91e86d970dc4ceb3af7f4cdc252673e2
jake <bigjake2s@gmail.com>
2024-04-18 07:53:04 -0400

    feat: switching to a Mux interface

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,6 +2,11 @@ package handlers
 
 import "net/http"
 
-func ApplyHandlers(mux *http.ServeMux) {
+type Mux interface {
+	Handle(pattern string, handler http.Handler)
+	HandleFunc(pattern string, handler http.HandlerFunc)
+}
+
+func Apply(mux Mux) {
 	mux.HandleFunc("GET /admin/login", adminLogin)
 }
