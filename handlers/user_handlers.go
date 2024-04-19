787c8c30ee75a9fea9fe56842c1e49e9f84c7ab9
jake <bigjake2s@gmail.com>
2024-04-19 12:39:53 -0400

    feat: porting login to use Page

--- handlers/user_handlers.go
+++ handlers/user_handlers.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"html/template"
 	"log/slog"
 	"net/http"
 
@@ -12,54 +11,53 @@ import (
 	"github.com/jbaikge/sparky/models/user"
 	"github.com/jbaikge/sparky/modules/database"
 	"github.com/jbaikge/sparky/modules/middleware"
+	"github.com/jbaikge/sparky/modules/page"
 	"github.com/jbaikge/sparky/modules/password"
 )
 
 func adminLogin(w http.ResponseWriter, r *http.Request) {
-	tpl := r.Context().Value(middleware.ContextTemplate).(*template.Template)
-	tpl.ExecuteTemplate(w, "user/login", nil)
+	p := page.New(r.Context())
+	p.Data["Title"] = "Login"
+	p.Render(w, "user/login")
 }
 
 func adminLoginAuth(w http.ResponseWriter, r *http.Request) {
-	type Repost struct {
-		Error string
-		Email string
-	}
+	var err error
+	p := page.New(r.Context())
 
-	repost := Repost{
-		Email: r.PostFormValue("email"),
-	}
+	email := r.PostFormValue("email")
 
-	defer func(repost *Repost) {
-		if repost.Error != "" {
-			slog.Warn("invalid login", "email", repost.Email, "error", repost.Error)
+	defer func(p *page.Page) {
+		if err != nil {
+			slog.Warn("invalid login", "email", email, "error", err)
+			p.Data["Error"] = err.Error()
 		}
-		tpl := r.Context().Value(middleware.ContextTemplate).(*template.Template)
-		tpl.ExecuteTemplate(w, "user/login-form", repost)
-	}(&repost)
+		p.Data["Email"] = email
+		p.Render(w, "user/login-form")
+	}(p)
 
 	db := r.Context().Value(middleware.ContextDatabase).(database.Database)
 	userRepo := user.NewUserRepository(db)
 
-	user, err := userRepo.GetUserByEmail(r.Context(), repost.Email)
+	user, err := userRepo.GetUserByEmail(r.Context(), email)
 	if errors.Is(err, sql.ErrNoRows) {
-		repost.Error = "Invalid email address"
+		err = fmt.Errorf("Invalid email address")
 		return
 	} else if err != nil {
-		repost.Error = fmt.Sprintf("Error from the database: %v", err)
+		err = fmt.Errorf("Error from the database: %w", err)
 		return
 	}
 
 	pw := r.PostFormValue("password")
 	if !password.Validate(user.Password, pw) {
-		repost.Error = "Invalid password"
+		err = fmt.Errorf("Invalid password")
 		return
 	}
 
 	sessionRepo := session.NewSessionRepository(db)
 	sessionId, err := sessionRepo.NewSession(r.Context(), user.UserId)
 	if err != nil {
-		repost.Error = fmt.Sprintf("Unable to create new session: %v", err)
+		err = fmt.Errorf("Unable to create new session: %w", err)
 		return
 	}
 
