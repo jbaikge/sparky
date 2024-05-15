29ff96e6e0f9a1b042d3be550da39f9da04b1324
jake <bigjake2s@gmail.com>
2024-05-15 10:29:49 -0400

    feat: theme selection capabilities

--- assets/js/color-scheme.js
+++ assets/js/color-scheme.js
@@ -1,12 +1,48 @@
+// Adapted from the shoelace.style docs.js
 (function() {
-  function setColorScheme(e) {
-    if (e.matches) {
-      document.documentElement.classList.add("dark", "sl-theme-dark");
-    } else {
-      document.documentElement.classList.remove("dark", "sl-theme-dark");
-    }
+  function getThemePreference() {
+    return localStorage.getItem("theme") || "auto";
   }
-  const darkModePreference = window.matchMedia("(prefers-color-scheme: dark)");
-  darkModePreference.addEventListener("change", setColorScheme);
-  setColorScheme(darkModePreference);
+
+  function setThemePreference(newTheme) {
+    localStorage.setItem("theme", newTheme);
+    updateThemeMenu(newTheme);
+    document.documentElement.classList.toggle("sl-theme-dark", prefersDark(newTheme));
+  }
+
+  function prefersDark(theme) {
+    if (theme == "auto") {
+      return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    }
+    return theme == "dark";
+  }
+
+  function updateThemeMenu(theme) {
+    const menu = document.querySelector("#theme-selector sl-menu");
+    if (menu == null) return;
+    Array.from(menu.querySelectorAll("sl-menu-item")).map(function(item) {
+      item.checked = (item.getAttribute("value") == theme);
+    });
+  }
+
+  // Listen for opening the dropdown
+  document.addEventListener("sl-show", function(event) {
+    const themeSelector = event.target.closest("#theme-selector");
+    if (themeSelector == null) return;
+    updateThemeMenu(getThemePreference());
+  });
+
+  // Listen for selections
+  document.addEventListener("sl-select", function(event) {
+    const menu = event.target.closest("#theme-selector sl-menu");
+    if (menu == null) return;
+    setThemePreference(event.detail.item.value);
+  });
+
+  // Listen for system theme changes
+  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function() {
+    setThemePreference(getThemePreference());
+  });
+
+  setThemePreference(getThemePreference());
 })();
